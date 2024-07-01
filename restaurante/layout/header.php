659abba23dfeb3f8f10d57c2c367852005550e3d
aitor <aitorjob69@gmail.com>
2024-07-01 11:42:07 +0200

    Update: navbar added.

--- restaurante/layout/header.php
+++ restaurante/layout/header.php
@@ -6,6 +6,27 @@
         <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
-        
+        <div class="navbar">
+        <span class="menu-icon" onclick="openNav()">&#9776;</span>
+        <a href="#home">Inicio</a>
+        </div>
+
+            <div id="myOffcanvas" class="offcanvas">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#home">Inicio</a>
+                <a href="#about">Acerca de</a>
+                <a href="#services">Servicios</a>
+                <a href="#contact">Contacto</a>
+            </div>
+
+        <script>
+            function openNav() {
+                document.getElementById("myOffcanvas").style.width = "250px";
+            }
+
+            function closeNav() {
+                document.getElementById("myOffcanvas").style.width = "0";
+            }
+        </script>
     </body>
 </html>
