6f0b0580b9beafa875544a5c7e1c3204fd5db22c
aitor <aitorjob69@gmail.com>
2024-07-02 10:02:02 +0200

    new php file created.

---
+++ restaurante/php/requisiciones.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Requisiciones</title>
+        <link rel="icon" type="image/png" href="../img/1.png">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <?php require_once "../layout/header.php" ?>
+    </body>
+</html>
