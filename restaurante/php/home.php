629685a991f842a0422c9ecd96d95030ffee64b9
aitor <aitorjob69@gmail.com>
2024-07-03 09:25:17 +0200

    Update: link icon & CSS, hide messages and picture with 3 images.

--- restaurante/php/home.php
+++ restaurante/php/home.php
@@ -3,9 +3,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Home</title>
+        <link rel="icon" type="image/png" href="../img/1.png">
+        <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
+        <!-- HEADER -->
         <?php require_once "../layout/header.php"; ?>
+
+        <!-- IMAGES FOR PICTURES -->
+        <picture class="home-img">
+            <img src="../img/plato1.jpg" alt="ES UN PLATO DE SALMÓN">
+            <img src="../img/plato2.jpg" alt="ES UN PLATO DE CARNE">
+            <img src="../img/plato3.jpg" alt="ES UN PLATO DE PIZZA CON CROQUETAS">
+        </picture>
+
     </body>
 </html>
