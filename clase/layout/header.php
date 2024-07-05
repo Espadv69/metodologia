edddb43acddb0d21968d7d31cd0247eeadc564f0
aitor <aitorjob69@gmail.com>
2024-07-05 13:52:45 +0200

    Update: src javascript and icons for link into navbar.

--- clase/layout/header.php
+++ clase/layout/header.php
@@ -12,13 +12,13 @@
         <div class="dropdown">
             <button onclick="toggleMenu()" class="dropbtn btn"><i class="bi bi-list"></i>Menu</button>
             <div id="myDropdown" class="dropdown-content">
-                <a href="#link1">Link 1</a>
-                <a href="#link2">Link 2</a>
-                <a href="#link3">Link 3</a>
-                <a href="#link3">Link 3</a>
+                <a href="#link1"><i class="bi bi-person-fill-add"></i> Registro estudiantes</a>
+                <a href="#link2"><i class="bi bi-person-vcard-fill"></i> Consultar estudiantes</a>
+                <a href="#link3"><i class="bi bi-person-fill-add"></i> Registro profesores</a>
+                <a href="#link3"><i class="bi bi-card-checklist"></i> Registro de notas</a>
             </div>
         </div>
-        <a href="#"><i class="bi bi-house-door-fill"></i>INICIO</a>
+        <a href="#" class="a btn"><i class="bi bi-house-door-fill"></i> INICIO</a>
     </div> 
     <script src="../js/navbar.js"></script>
     </body>
