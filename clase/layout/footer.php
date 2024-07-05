3353f03613a062fcebff87ba1c2b73233174e7a2
aitor <aitorjob69@gmail.com>
2024-07-05 14:20:10 +0200

    Update: structure html and footer addeed

---
+++ clase/layout/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Footer</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+    <body>
+        
+    <div class="footer-lay">
+        <footer>
+            esto es el footer
+        </footer>
+    </div>
+
+    </body>
+</html>
