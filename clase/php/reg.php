12a21f406297792125dd2e0f40a66e947c164180
aitor <aitorjob69@gmail.com>
2024-07-08 09:59:30 +0200

    Update: link for styles and name of inputs were added.

--- clase/php/reg.php
+++ clase/php/reg.php
@@ -4,13 +4,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Registro</title>
+        <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
         <form action="../back-php/reg-backend.php" method="post" class="log-reg-form">
-            <input type="text" name="" placeholder="" required>
-            <input type="text" name="" placeholder="" required>
-            <input type="email" name="" placeholder="" required>
-            <input type="password" name="" placeholder="" required>
+            <input type="text" name="nombre" placeholder="Ingrese su nombre" required>
+            <input type="text" name="usuario" placeholder="Ingrese su nombre de usuario" required>
+            <input type="email" name="email" placeholder="nombre@ejemplo.com" required>
+            <input type="password" name="pass" placeholder="Ingrese una contraseña" required>
             <button>REGISTRAR</button>
         </form>
     </body>
