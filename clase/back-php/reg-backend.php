09ebbf5bf0691aa9dd9d203efaf7baacd2e08c98
aitor <aitorjob69@gmail.com>
2024-07-08 09:24:52 +0200

    Update: insert into table.

--- clase/back-php/reg-backend.php
+++ clase/back-php/reg-backend.php
@@ -4,3 +4,11 @@ $nombre = $_POST["nombre"];
 $usuario = $_POST["usuario"];
 $email = $_POST["email"];
 $pass = $_POST["pass"];
+
+// INSERT
+$sql = ("INSERT INTO sesion(nombre, usuario, email, pass) VALUES (
+    '$nombre',
+    '$usuario',
+    '$email',
+    '$pass'
+)");
