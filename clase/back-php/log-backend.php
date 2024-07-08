01cb8b0db0b4162dc0e799eca1d1dacef12d230f
aitor <aitorjob69@gmail.com>
2024-07-08 10:22:53 +0200

    Update: variable begin to start the page.

--- clase/back-php/log-backend.php
+++ clase/back-php/log-backend.php
@@ -6,3 +6,16 @@ require_once "./conn.php";
 $email = $_POST["email"];
 $pass = $_POST["pass"];
 
+// BEGIN
+$begin = mysqli_query($conn, "SELECT * FROM sesion WHERE email='$email' AND pass='$pass'");
+
+if(mysqli_num_rows($begin) > 0) {
+    header("location: ../php/inscripcion.php");
+    exit;
+} else {
+    echo "
+    <script>
+        alert('Email o contraseña inconrrectos❗');
+        window.location = '../index.php';
+    </script>";
+}
