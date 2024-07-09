6ba1bbbe12449cb6da79d2c70cfcec381f49a7c4
aitor <aitorjob69@gmail.com>
2024-07-09 11:03:13 +0200

    Update: table for reg notas

--- clase/php/reg-notas.php
+++ clase/php/reg-notas.php
@@ -8,6 +8,32 @@
     </head>
     <body>
         <?php require_once "../layout/header.php" ?>
+        <table class="table-notas">
+            <thead>
+                <tr>
+                    <th>Módulos</th>
+                    <th>Horas Módulos</th>
+                    <th>Notas Final</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>item1</td>
+                    <td>item2</td>
+                    <td>item3</td>
+                </tr>
+                <tr>
+                    <td>item1</td>
+                    <td>item2</td>
+                    <td>item3</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="btn-notas">
+            <button onclick="media()">Add</button>
+            <button onclick="reset()">Reset</button>
+        </div>
         <?php require_once "../layout/footer.php" ?>
+        <script src="../js/notas.js"></script>
     </body>
 </html>
