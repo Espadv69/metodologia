6ed1fd8b3b5ba8e84d938a63ebb4147319763e63
aitor <aitorjob69@gmail.com>
2024-07-09 14:24:17 +0200

    Update: information and classes for table-notas were added.

--- clase/js/notas.js
+++ clase/js/notas.js
@@ -1,5 +1,7 @@
 function media() {
-    const nombre = prompt("Ingrese el nombre del estudiante")
+    const nombre = prompt("Ingrese el nombre del estudiante");
+    const modulo = prompt("Ingrese módulo");
+    const hora_modulo = parseInt(prompt("Ingrese horas del módulo"));
     const nota1 = parseFloat(prompt("Ingrese su primera nota."));
     const nota2 = parseFloat(prompt("Ingrese su segunda nota."));
     const nota3 = parseFloat(prompt("Ingrese su tercera nota."));
@@ -7,9 +9,12 @@ function media() {
     if(!isNaN(nota1) && !isNaN(nota2) && !isNaN(nota3)) {
         if(!(nota1 > 10) && !(nota2 > 10) && !(nota3 > 10)) {
             const total = (nota1 + nota2 + nota3)/3;
+
+            document.querySelector(".modulo").innerHTML = modulo;
+
+            document.querySelector(".hora_modulo").innerHTML = `${hora_modulo} Horas`;
         
-            alert(`La media de las tres notas de (${nombre}) es: ${total.toFixed(2)}`);
-            console.log(total.toFixed(2));
+            document.querySelector(".nota").innerHTML = (`La media de ( <span class="name">${nombre}</span> ) es: ${total.toFixed(2)}`);
         } else {
             alert("algun numero añadido es mayor a 🔟")
         }
