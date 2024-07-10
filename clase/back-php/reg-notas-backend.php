18826d69c590aa522b2d28279bb6ebf2fb368b99
aitor <aitorjob69@gmail.com>
2024-07-10 09:23:34 +0200

    Update: check id for tabla notas

--- clase/back-php/reg-notas-backend.php
+++ clase/back-php/reg-notas-backend.php
@@ -14,5 +14,16 @@ $sql = "INSERT INTO notas(nombre, id, apto) VALUES (
     '$apto'
 )";
 
+// CHECK ID
+$check_id = mysqli_query($conn, "SELECT * FROM notas WHERE id='$id'");
+if(mysqli_num_rows($check_id) > 0) {
+    echo "
+    <script>
+        alert('Ya hay una persona con ese número de identificación❌');
+        window.location = '../php/reg-notas.php';
+    </script>";
+    exit;
+}
+
 // INSERT INTO
 $insert = mysqli_query($conn, $sql);
