fd84633b35987acb3844254fb6cb12f325cd1f2e
aitor <aitorjob69@gmail.com>
2024-07-10 09:40:56 +0200

    Update: hide message for user

--- clase/index.php
+++ clase/index.php
@@ -7,12 +7,12 @@
         <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
+        <!-- Form login -->
         <form action="./back-php/log-backend.php" method="post" class="log-reg-form">
             <input type="email" name="email" placeholder="nombre@ejemplo.com" required>
             <input type="password" name="pass" placeholder="Ingrese una contraseña" required>
             <button>Entrar</button>
             <p>Si aún no estas registrado <a href="./php/reg.php">pinche aquí</a> ⬅️</p>
         </form>
-
     </body>
 </html>
