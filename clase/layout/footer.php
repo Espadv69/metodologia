70458b5c97a147435e55b7bbcf8c8920df4463e5
espadv69 <aitorjob69@gmail.com>
2024-07-11 21:23:40 +0200

    Update: script called footer.js and factura modal

--- clase/layout/footer.php
+++ clase/layout/footer.php
@@ -6,6 +6,40 @@
         <title>Footer</title>
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <style>
+            .modal {
+                display: none;
+                position: fixed;
+                z-index: 1;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                overflow: auto;
+                background-color: rgb(0,0,0);
+                background-color: rgba(0,0,0,0.4);
+                padding-top: 60px;
+            }
+            .modal-content {
+                background-color: #fefefe;
+                margin: 5% auto;
+                padding: 20px;
+                border: 1px solid #888;
+                width: 80%;
+            }
+            .close {
+                color: #aaa;
+                float: right;
+                font-size: 28px;
+                font-weight: bold;
+            }
+            .close:hover,
+            .close:focus {
+                color: black;
+                text-decoration: none;
+                cursor: pointer;
+            }
+        </style>
     </head>
     <body>
         
@@ -16,12 +50,23 @@
             </div>
             <h4>Nuestros cursos</h4>
             <div class="footer-items">
-                <a href="item"><i class="bi bi-filetype-html"></i> HTML</a>
-                <a href="item"><i class="bi bi-filetype-css"></i> CSS</a>
-                <a href="item"><i class="bi bi-filetype-js"></i> JS</a>
-                <a href="item"><i class="bi bi-filetype-php"></i> PHP</a>
+                <a class="course-link" data-course="HTML"><i class="bi bi-filetype-html"></i> HTML</a>
+                <a class="course-link" data-course="CSS"><i class="bi bi-filetype-css"></i> CSS</a>
+                <a class="course-link" data-course="JS"><i class="bi bi-filetype-js"></i> JS</a>
+                <a class="course-link" data-course="PHP"><i class="bi bi-filetype-php"></i> PHP</a>
             </div>
         </footer>
+
+            <!-- The Modal -->
+        <div id="invoiceModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h2>Factura</h2>
+                <p id="invoiceContent">Aquí va el contenido de la factura</p>
+            </div>
+        </div>
+
+        <script src="../js/footer.js"></script>
 
     </body>
 </html>
