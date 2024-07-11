2ab5a21903cbc122150ca7aa1a35df2afac650ec
espadv69 <aitorjob69@gmail.com>
2024-07-11 22:00:10 +0200

    Update: structure of html, require header and footer

---
+++ clase/php/cursos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cursos</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <?php require_once "../layout/header.php" ?>
+        <?php require_once "../layout/footer.php" ?>
+    </body>
+</html>
