69a6caaf429402f11081fd2ad7c8b75e609a9bab
espadv69 <aitorjob69@gmail.com>
2024-07-12 17:52:45 +0200

    sql for class

---
+++ escuela/sql/compra.sql
@@ -0,0 +1,10 @@
+use metodologia
+
+CREATE TABLE compra(
+    nombre VARCHAR(20),
+    email VARCHAR(30),
+    numero_cuenta INT(22),
+    cvv INT(3),
+    caducidad DATE,
+    cursos VARCHAR(20)
+)
