08c1b006c7f658cbcbc2be53b2181b2c08daf422
aitor <aitorjob69@gmail.com>
2024-07-12 13:52:04 +0200

    new folder name

---
+++ escuela/php/alumno.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alumno</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <style>
+            body {
+                background-color: #121212;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="div-alert">
+            <?php 
+                echo "Has recibido un mensaje del profesor: <br>";
+                echo "<span class='span-comentario'><span style='color: red'>Identificación: </span>" . $_REQUEST["id"] . "<br><span style='color: red'>Mensage: </span>" . $_REQUEST["texto"] . "</span>";
+            ?>
+            <span class="cerrar" onclick="cerrarAlerta()">X</span>
+        </div>
+
+        <script>
+            function cerrarAlerta() {
+                let alerta = document.querySelector(".div-alert");
+                alerta.style.display = "none";
+            }
+        </script>
+    </body>
+</html>
