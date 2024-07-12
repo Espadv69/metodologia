169237d94d5f2680d38d3496750685e192e87a90
aitor <aitorjob69@gmail.com>
2024-07-12 13:48:40 +0200

    folder name was changed

--- clase/back-php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// VARIABLES
-$host = "localhost";
-$root = "root";
-$password = "";
-$dbname = "metodologia";
-
-// CONECCTION
-$conn = mysqli_connect($host, $root, $password, $dbname) or die ("had been an error " . mysqli_errno($conn));
