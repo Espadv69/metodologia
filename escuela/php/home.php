812fb47ca9efc1d14da1c19eca904a4062adb89a
aitor <aitorjob69@gmail.com>
2024-07-12 13:52:11 +0200

    new folder name

---
+++ escuela/php/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <?php require_once "../layout/header.php" ?>
+
+        <div class="img-head">
+            <img src="../img/head-home.png" alt="ES UN LOGO DE ( </> ) PROGRAMACIÓN">
+        </div>
+
+        <h4>Bienvenido a nuestra escuela</h4><br><br>
+
+        <div class="flex-home">
+            <img src="../img/home-1.png" alt="SE VERÍA MUCHAS CARTAS VOLANDO A UNA PANTALLA">
+            <img src="../img/home-2.jpg" alt="SE VERÍA ESCRITO EN LA FOTO PROGRAMMER">
+            <img src="../img/home-3.jpg" alt="SE VERÍA A UN SEÑOR RECOSTADO EN UNA MESA ENFRENTE DE SU ORDENADOR">
+            <img src="../img/home-4.jpg" alt="SE VERÍA LAS MANOS DE UN PROGRAMADOR PROGRAMANDO">
+        </div><br><br>
+
+        <?php require_once "../layout/footer.php" ?>
+    </body>
+</html>
