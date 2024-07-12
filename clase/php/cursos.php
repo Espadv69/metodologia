42e75b2c3e455d91ac1c751fd4991da69e526194
aitor <aitorjob69@gmail.com>
2024-07-12 09:24:03 +0200

    Update: structure of form

--- clase/php/cursos.php
+++ clase/php/cursos.php
@@ -8,6 +8,21 @@
     </head>
     <body>
         <?php require_once "../layout/header.php" ?>
+        <form action="" class="form-cursos">
+            <input type="text" name="nombre" placeholder="Ingrese su nombre" required>
+            <input type="email" name="email" placeholder="nombre@ejemplo.com" required>
+            <select name="select" required>
+                <option disabled selected name="cursos">Cursos</option>
+                <option value="HTML">HTML</option>
+                <option value="CSS">CSS</option>
+                <option value="JS">JS</option>
+                <option value="PHP">PHP</option>
+            </select>
+            <div class="btn-cursos">
+                <button type="reset">Borrar</button>
+                <button>Comprar</button>
+            </div>
+        </form>
         <?php require_once "../layout/footer.php" ?>
     </body>
 </html>
