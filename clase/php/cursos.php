c9e5347962978c3c9a8726fb56aa34478463db9b
aitor <aitorjob69@gmail.com>
2024-07-12 09:57:49 +0200

    Update: changes for values of options

--- clase/php/cursos.php
+++ clase/php/cursos.php
@@ -8,21 +8,21 @@
     </head>
     <body>
         <?php require_once "../layout/header.php" ?>
+        <h4>SELECCIONE SU CURSO</h4>
         <form action="" class="form-cursos">
             <input type="text" name="nombre" placeholder="Ingrese su nombre" required>
             <input type="email" name="email" placeholder="nombre@ejemplo.com" required>
             <select name="select" required>
                 <option disabled selected name="cursos">Cursos</option>
-                <option value="HTML">HTML</option>
-                <option value="CSS">CSS</option>
-                <option value="JS">JS</option>
-                <option value="PHP">PHP</option>
+                <option value="HTML 50€">HTML</option>
+                <option value="CSS 75€">CSS</option>
+                <option value="JS 100€">JS</option>
+                <option value="PHP 90€">PHP</option>
             </select>
             <div class="btn-cursos">
-                <button type="reset">Borrar</button>
-                <button>Comprar</button>
+                <button><i class="bi bi-cart-check-fill"></i> Comprar</button>
+                <button type="reset"><i class="bi bi-trash-fill"> </i>Borrar</button>
             </div>
         </form>
-        <?php require_once "../layout/footer.php" ?>
     </body>
 </html>
