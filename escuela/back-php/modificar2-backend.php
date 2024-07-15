3fa0c2902262cbdaf90d1d810c19f2ace294ef90
aitor <aitorjob69@gmail.com>
2024-07-15 14:13:18 +0200

    Update: structure of html was delete

--- escuela/back-php/modificar2-backend.php
+++ escuela/back-php/modificar2-backend.php
@@ -2,16 +2,13 @@
 // CONNECTION
 require_once "./conn.php";
 
-?>
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Modificar</title>
-        <link rel="stylesheet" href="../css/style.css">
-    </head>
-    <body>
-        
-    </body>
-</html>
+// UPDATE TABLE
+$update = mysqli_query($conn, "UPDATE notas SET id='$_REQUEST[notaNueva]' WHERE id='$_REQUEST[nota]'");
+
+if($update) {
+    echo "
+    <script>
+        alert('El usuario fue modificado');
+        window.location = '../php/modificar.php';
+    </script>";
+}
