3866d21a0b0960c211b3083f3d1c114b90f36ce5
aitor <aitorjob69@gmail.com>
2024-07-17 12:26:41 +0200

    Update: function for span was deleted

--- escuela/js/horario.js
+++ escuela/js/horario.js
@@ -4,17 +4,9 @@ let modal = document.getElementById("myModal");
 // Obtener el botón que abre el modal
 let btn = document.getElementById("openModalBtn");
 
-// Obtener el elemento <span> que cierra el modal
-let span = document.getElementsByClassName("close")[0];
-
 // Cuando el usuario haga clic en el botón, abrir el modal 
 btn.onclick = function() {
     modal.style.display = "block";
-}
-
-// Cuando el usuario haga clic en <span> (x), cerrar el modal
-span.onclick = function() {
-    closeModal();
 }
 
 // Cuando el usuario haga clic en cualquier lugar fuera del modal, cerrarlo
@@ -26,9 +18,9 @@ window.onclick = function(event) {
 
 // Función para cerrar el modal con animación
 function closeModal() {
-    modal.style.animation = "fadeOut 0.5s";
+    modal.style.animation = "fadeOut 0.3s";
     setTimeout(function() {
         modal.style.display = "none";
-        modal.style.animation = "fadeIn 0.5s";
-    }, 500);
+        modal.style.animation = "fadeIn 0.3s";
+    }, 300);
 }
