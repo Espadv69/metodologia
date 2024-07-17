882d6a2d7e9947da7e93ae97c85e8ba76ad75ff8
aitor <aitorjob69@gmail.com>
2024-07-17 10:35:01 +0200

    Update: header, footer and <h4>

---
+++ escuela/php/horario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Horario</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <?php require_once "../layout/header.php" ?>
+        <br>
+
+        <h4>CORDINACIÓN DE HORARIO</h4>
+        
+
+        <br>
+        <?php require_once "../layout/footer.php" ?>
+    </body>
+</html>
