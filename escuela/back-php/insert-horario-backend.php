283fa1192f40b2c3e4d1f60ecd64e16d2d0d290f
aitor <aitorjob69@gmail.com>
2024-07-17 13:24:23 +0200

    Update: bindparam with objects

--- escuela/back-php/insert-horario-backend.php
+++ escuela/back-php/insert-horario-backend.php
@@ -6,9 +6,18 @@ ob_start();
 require_once "./horario-backend.php";
 
 // CONDITION
-$nombre = htmlentities($_POST["nombres"]);
-$apelllidos = htmlentities($_POST["apelllidos"]);
+$nombres = htmlentities($_POST["nombres"]);
+$apellidos = htmlentities($_POST["apellidos"]);
 $horario = htmlentities($_POST["horario"]);
 $programas = addslashes(implode(', ', $_POST['programas']));
 
 $query = $db -> prepare("INSERT INTO personal(`nombres`, `apellidos`, `horario`, `programas`) VALUES (:nombres, :apellidos, :horario, :programas)");
+
+$query -> bindParam(":nombres", $nombres);
+$query -> bindParam(":apellidos", $apellidos);
+$query -> bindParam(":horario", $horario);
+$query -> bindParam(":programas", $programas);
+
+$query -> execute();
+
+header("location: ../php/horario.php");
