e8e96d946a6d158abfd6685f44ecc2dd97257370
aitor <aitorjob69@gmail.com>
2024-07-22 14:21:51 +0200

    Update: new method

--- youtube/js/navbar.js
+++ youtube/js/navbar.js
@@ -1,10 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.getElementById("menu-icon");
-    const sideMenu = document.getElementById("side-menu");
-
-    menuIcon.addEventListener("click", () => {
-        menuIcon.classList.toggle("open");
-        sideMenu.classList.toggle("open");
-    });
+document.getElementById('menu-icon').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.getElementById('side-menu').classList.toggle('open');
 });
-
