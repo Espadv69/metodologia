920f8ce7e3b0f01014a63b77c6b0ae1df9d60cb7
aitor <aitorjob69@gmail.com>
2024-07-22 14:02:08 +0200

    Update: icons for nav

--- youtube/layout/header.php
+++ youtube/layout/header.php
@@ -4,19 +4,38 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
     <body>
-        <div class="menu-icon" id="menu-icon">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
+
+        <div class="all-nav-yt">
+            <div style="display: flex; gap: 1rem;">
+                <div class="menu-icon" id="menu-icon">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+                <img src="https://www.youtube.com/img/desktop/yt_1200.png" alt="" width="50px">
+            </div>
+            <div class="side-menu" id="side-menu">
+                <div style="display: flex; justify-content: center;">
+                    <img src="./img/yt.png" alt="" width="100px" >
+                </div>
+
+                <a href="http://localhost/metodologia/youtube/index.php"><i class="bi bi-house-fill"></i> Home</a>
+            </div>
+
+            <div>
+                <input type="text" placeholder="search"><i class="bi bi-search i"></i>
+            </div>
+
+            <div class="icons">
+                <a href="#"><i class="bi bi-bell"></i></a>
+                <a href="#"><i class="bi bi-camera-video"></i></a>
+                <a href="#"><i class="bi bi-person-circle"></i></a>
+            </div>
         </div>
-        <div class="side-menu" id="side-menu">
-            <a href="#">Inicio</a>
-            <a href="#">Sobre Nosotros</a>
-            <a href="#">Servicios</a>
-            <a href="#">Contacto</a>
-        </div>
+
         <script src="./js/navbar.js"></script>
     </body>
 </html>
