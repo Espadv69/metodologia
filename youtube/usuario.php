094705e634e72ec8ef49cdb6d59c8d7294da1cc9
aitor <aitorjob69@gmail.com>
2024-07-23 12:02:55 +0200

    Update: page for user

---
+++ youtube/usuario.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php require_once "./layout/header.php" ?>
+
+    <!-- HEADER -->
+    <div class="header-user">
+        <img src="./img/yt.gif" alt="">
+    </div>
+
+    <!-- INFO USER -->
+    <div class="all-user">
+        <div class="info-profile">
+            <!-- IMG PROFILE -->
+            <img src="./img/profile.jpg" alt="">
+            <div class="info">
+                <!-- INFO -->
+                <h2>Espadv69</h2>
+                <h5>@espadv._</h5>
+                <p style="cursor: pointer;">Más sobre este canal...</p>
+                <!-- LINKS -->
+                <div class="links">
+                    <a href="#">Personalizar canal</a>
+                    <a href="#">Gestionar videos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- VIDEO LINKS -->
+    <div class="video-links">
+        <a href="#">Inicio</a>
+        <a href="#" class="bi bi-search"></a>
+    </div>
+
+    <div class="img-empty">
+        <img src="https://i.pinimg.com/originals/aa/f7/05/aaf705e06726ce3881288ae4be3ac5fe.jpg" alt="">
+        <h6>Crear Contenido</h6>
+        <p>Upload and record at home or on the go. Everything you make public will appear here.</p>
+        <button>Crear</button>
+    </div><br><br><br>
+
+
+</body>
+</html>
