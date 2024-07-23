618d82e90fb73cb02c830495a8f758e64cd00ec6
aitor <aitorjob69@gmail.com>
2024-07-23 10:38:54 +0200

    Update: new anonimous function for side-right navbar

--- youtube/js/navbar.js
+++ youtube/js/navbar.js
@@ -2,3 +2,11 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     this.classList.toggle('open');
     document.getElementById('side-menu').classList.toggle('open');
 });
+
+document.querySelector('.open-menu').addEventListener('click', function () {
+    document.getElementById('myMenu').style.width = '250px';
+});
+
+function closeMenu() {
+    document.getElementById('myMenu').style.width = '0';
+}
