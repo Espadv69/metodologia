e7c6073185c7ba8f5e561d9db7d667094fa28ea7
aitor <aitorjob69@gmail.com>
2024-07-24 11:33:12 +0200

    Update: table video was created

---
+++ youtube/db/video.sql
@@ -0,0 +1,7 @@
+use metodologia
+
+CREATE TABLE video(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    ubicacion VARCHAR(255) NOT NULL
+)ENGINE = innoDB DEFAULT CHARSET = utf8
