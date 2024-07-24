f64ec0e09a91160e8e087a44976fb837a69ba5e6
aitor <aitorjob69@gmail.com>
2024-07-24 12:21:19 +0200

    Update: dbname variable into try was change

--- youtube/back-end/conn.php
+++ youtube/back-end/conn.php
@@ -7,8 +7,8 @@ $dbname = "metodologia";
 
 // TRY CONNECT
 try {
-    $dbname = new PDO("mysql: host={$host}; dbname={$dbname}", $root, $password);
-    $dbname -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO("mysql: host={$host}; dbname={$dbname}", $root, $password);
+    $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("CONNECT ERROR " . $e -> getMessage());
 }
