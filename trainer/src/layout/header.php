73d72085eadafd5986c45df5bfdc8cc003114e8d
aitor <aitorjob69@gmail.com>
2024-07-29 13:45:17 +0200

    Update: link css fixed

--- trainer/src/layout/header.php
+++ trainer/src/layout/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./css/style_navbar.css">
+<link rel="stylesheet" href="../css/style_navbar.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 <nav class="nav">
