649c90858304fd307739507a2114eb41f0e62e03
aitor <aitorjob69@gmail.com>
2024-07-29 10:03:09 +0200

    Update: strcuture of navbar

--- trainer/layout/header.php
+++ trainer/layout/header.php
@@ -1,1 +1,10 @@
 <link rel="stylesheet" href="../css/style_navbar.css">
+
+<nav class="nav">
+    <ul class="nav__ul">
+        <li class="nav__li"><a href=""></a></li>
+        <li class="nav__li"><a href=""></a></li>
+        <li class="nav__li"><a href=""></a></li>
+        <li class="nav__li"><a href=""></a></li>
+    </ul>
+</nav>
