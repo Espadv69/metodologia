71938527059b545e2eed64508395da9f8e7c7519
aitor <aitorjob69@gmail.com>
2024-07-29 13:42:55 +0200

    Update: items for nav

--- trainer/src/layout/header.php
+++ trainer/src/layout/header.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" href="./css/style_navbar.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 <nav class="nav">
     <ul class="nav__ul">
-        <li class="nav__li"><a href="">Link</a></li>
-        <li class="nav__li"><a href="">Link</a></li>
-        <li class="nav__li"><a href="">Link</a></li>
-        <li class="nav__li"><a href="">Link</a></li>
+        <li class="nav__li home"><a href=""><i class="bi bi-house-fill"> Home</i></a></li>
+        <li class="nav__li"><a href=""><i class="bi bi-person-arms-up"></i> Cliente</a></li>
+        <li class="nav__li"><a href=""><i class="bi bi-person-walking"></i> Entrenador</a></li>
     </ul>
 </nav>
