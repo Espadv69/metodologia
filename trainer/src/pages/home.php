3c59c5bed8356b7694973a7cb4d6ef92997d503a
aitor <aitorjob69@gmail.com>
2024-07-31 12:11:40 +0200

    Update: my logo in the middle of the vh

--- trainer/src/pages/home.php
+++ trainer/src/pages/home.php
@@ -5,11 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
         <link rel="stylesheet" href="../css/style.css">
+        <style>
+            .body {
+                overflow-y: hidden;
+            }
+            .img-header {
+                min-height: 88.4dvh;
+                display: grid;
+                place-content: center;
+                padding: .2rem;
+                backdrop-filter: blur(5px);
+                overflow-y: hidden;
+
+                img {
+                    border-radius: .5rem;
+                    box-shadow: 0px 0px 10px lightblue;
+                }
+            }
+        </style>
     </head>
-    <body>
+    <body class="body">
         <?php require_once "../layout/header.php" ?>
 
-            <h3 style="text-align: center;">Hola mundo</h3>
+        <div class="img-header">
+            <img src="../img/logo.png" alt="" width="100%" style="max-width: 300px;">
+        </div>
 
     </body>
 </html>
